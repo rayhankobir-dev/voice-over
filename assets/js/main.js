957f26ca0ab5fb761740e70acbb126e8bc1630f2
Raju Rayhan <rajurayhan@Rajus-MBP.W4>
2023-09-15 22:24:43 +0600

    navigation updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,4 +40,57 @@ $(document).ready(function () {
       });
     });
   });
+
+  Array.from($("#mobile-menu a")).forEach((a) => {
+    $(a).click(() => {
+      $("#mobile-menu").hide();
+    });
+  });
+
+  activeNavigation(Array.from($(".nav-items li a")));
+  activeNavigation(Array.from($("#mobile-menu a")));
+
+  function activeNavigation(items) {
+    items.forEach((a) => {
+      $(a).click(() => {
+        $(items).not(a).addClass("text-white");
+        $(a).removeClass("text-white");
+        $(a).addClass("text-blue-600");
+      });
+    });
+  }
+
+  window.addEventListener("resize", function () {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+      $("#mobile-menu").hide();
+    }
+  });
+
+  const observer = new IntersectionObserver(
+    (items) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          $("#mobile-menu a")
+            .not($('a[href="#' + item.target.id + '"]'))
+            .removeClass("text-blue-600");
+          $('#mobile-menu a[href="#' + item.target.id + '"]').addClass(
+            "text-blue-600"
+          );
+          $(".nav-items li a")
+            .not($(`.nav-items li a[href="#${item.target.id}"]`))
+            .removeClass("text-blue-600");
+          $(`.nav-items li a[href="#${item.target.id}"]`).addClass(
+            "text-blue-600"
+          );
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  observer.observe(document.getElementById("contact"));
 });
+
+function openMobileMenu() {
+  $("#mobile-menu").toggle();
+}
